5733d2e671373b0c6c436fb1f3561b017c3b18ce
Tanish Singh <tanishsingh866@gmail.com>
2026-03-15 09:03:09 +0000

    fixing server js file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//initializes
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
@@ -7,40 +6,39 @@ const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
 const myEnv = dotenv.config();
 dotenvExpand.expand(myEnv);
-var cookies = require("cookie-parser");
+var cookies = require('cookie-parser');
 
-//app
 const app = express();
 app.use(cookies());
 
-//port
 const port = process.env.PORT || 6400;
 
-//routes
 const productRoute = require('./routes/product');
 const homeRoute = require('./routes/home');
 const cartRoute = require('./routes/cart');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 
-//middleware
 const corsOptions = {
-	origin: ['https://your-app.vercel.app', 'http://localhost:5173'],
-	credentials: true,
-	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Authorization']
+  origin: [
+    'https://shop-cart-ts1442680-9411-tanish-singhs-projects-245eb38e.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
-  
+
 app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//view engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 app.disable('view cache');
 
 app.use('/', homeRoute);
@@ -50,25 +48,25 @@ app.use('/users', userRoute);
 app.use('/auth', authRoute);
 
 app.get('/accesstoken', (req, res) => {
-	console.log("cookie",req.cookies);
-	return res.json({token: req.cookies['jwt-token']});
+  console.log('cookie', req.cookies);
+  return res.json({ token: req.cookies['jwt-token'] });
 });
 
 app.get('/logout', (req, res) => {
-	res.clearCookie('jwt-token');
-	return res.status(200).json({msg: 'logout done'});
-})
+  res.clearCookie('jwt-token');
+  return res.status(200).json({ msg: 'logout done' });
+});
 
-//mongoose
 mongoose.set('useFindAndModify', false);
 mongoose.set('useUnifiedTopology', true);
+
 mongoose
-	.connect(import.meta.env.DATABASE_URL)
-	.then(() => {
-		app.listen(port, () => {
-			console.log('Server running on port', port);
-		});
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log('Server running on port', port);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
