9a57f63344fe0d4acc21d0e6e8497cf8819ad87a
Tanish Singh <tanishsingh866@gmail.com>
2026-03-15 08:34:21 +0000

    Fixing url

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const authRoute = require('./routes/auth');
 //middleware
 const corsOptions = {
 	origin: [
+		'http://localhost:5173',
 		'https://shop-cart-pi-eight.vercel.app/' // Replace with your React app's origin
 	],
 	credentials: true, // Enable credentials (cookies) in cross-origin requests
